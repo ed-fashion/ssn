e449e2cf426f9cb7a53de30522835a2877ae127b
ed-fashion <ed.ffashion@gmail.com>
2026-07-09 10:09:47 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// ============================================
-// SSN — Configuração Supabase
-// Substitui os valores abaixo pelos teus
-// ============================================
-
-const SSN_CONFIG = {
-  supabaseUrl: 'https://mekuafbbbjzsyfqgfall.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1la3VhZmJiYmp6c3lmcWdmYWxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1NTQwMjEsImV4cCI6MjA5OTEzMDAyMX0.u--7tokr3CIHTeJyaaVMIqCTEJH9fOPwVAfzofxBkjA'
-};
